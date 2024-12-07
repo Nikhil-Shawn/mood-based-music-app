9a94af3addd983ff0995554d9cf1f954cf97bb2d
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-12-07 23:05:40 +0100

    Created home page UI base for desktop with dummy data

--- mood-music-player/src/index.js
+++ mood-music-player/src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1DB954", // Spotify green
+    },
+    background: {
+      default: "#121212",
+      paper: "#181818",
+    },
+    text: {
+      primary: "#ffffff",
+      secondary: "#b3b3b3",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
